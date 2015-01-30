9c6ee678186a7253a9d9ee7828c7b30e140059b7
tomsumner <tom.sumner@lshtm.ac.uk>
2015-01-30 16:34:19 +0000

    Developed to try to model demography in SA in absence of disease. Need to expand to 1 year age bins

--- Age_structure_test.R
+++ Age_structure_test.R
@@ -1,51 +1,74 @@
-## Code to test approach for age structured model 
+## Code to test approach for age structured model - currently limited to trying to get the demography to work
 
 ## Based on http://kinglab.eeb.lsa.umich.edu/EEID/eeid/2011_eco/waifw.pdf
 
+## Set working directory
+setwd("C:/Users/TOM SUMMER/Documents/TIME_research/TeaTime")
+
+## Load packages
 library(deSolve)
+library(reshape2)
+library(ggplot2)
 
-ages <- seq(1,100,by=1) # upper end of age classes - 100 1 year age bins
-num_ages <- length(ages) # number of age classes
-da <- diff(c(0,ages)) # widths of age classes
+# Set up age structure
+ages <- c(seq(4,79,by=5),100) # upper end of age classes: 0-4 then five year bions to 79 then 80+
+num_ages <- length(ages) # calculates the number of age classes
+da <- diff(c(0,ages)) # calculates the widths of age classes
 
-## Initial conditions
-                                             
-yinit <- c(S=c(rep(100,50),90,rep(100,49)),
-           I=c(rep(0,50),10,rep(0,49)),
-           R=c(rep(0,100)))
-
+## Initial conditions - 1950 population 
+yinit <- c(S=c(2088,1706,1485,1340,1183,1040,960,835,725,589,502,415,324,228,148,77,37))
 sindex <- 1:num_ages
-iindex <- (num_ages+1):(2*num_ages)
-rindex <- (2*num_ages+1):(3*num_ages)
 
 ## To capture the aging process, define a matrix to hold the rates of movement between age classes.
 aging <- diag(-1/da)
 aging[row(aging)-col(aging)==1] <- 1/head(da,-1)
+aging[num_ages,num_ages]=0   # set rate out of last box = 0 - will account for flow out of here via death rate
 
-# Parameters
-beta <- 0.005
-gamma <- 10
-births <- 100
+## Mortality rates
+# Uses age specific LE tables from UN pop for South Africa, currently just the average over time, but could be made to vary with time
+LE<-c(55.00,50.60,46.02,41.64,37.51,33.66,29.94,26.44,23.01,19.73,16.65,13.75,11.22,9.04,7.20,5.62,3.09)
+death <- diag(1/LE)
 
-## Now we can put the pieces together to write a simulator for the age-structured SIR dynamics
+## Fertility
+# Uses crude birth rate (per 1000 population) from UN pop for South Africa, currently takes values at lower time band and linearly interpolates 
+births <- approxfun(x=c(1950,1955,1960,1965,1970,1975,1980,1985,1990,1995,2000,2005),y=c(44,42,41,38,38,36,34,31,27,25,24,22),rule=2)
+
+## Now we can put the pieces together to write a simulator for the demogrpahics
 ja.multistage.model <- function (t, x, ...) {
   s <- x[sindex] # susceptibles
-  i <- x[iindex] # infecteds
-  r <- x[rindex] # recovereds
-  lambda <- beta*i # force of infection
-  dsdt <- -lambda*s+aging%*%s
-  didt <- lambda*s+aging%*%i-gamma*i
-  drdt <- aging%*%r+gamma*i
-  dsdt[1] <- dsdt[1]+births
-  list(c(dsdt,didt,drdt))
+
+  Total <- sum(s)
+  
+  dsdt <- aging%*%s - death%*%s
+  
+  dsdt[1] <- dsdt[1]+births(t)*Total/1000
+  
+  list(c(dsdt),Total=Total)
 }
 
-# We can plug this into ode just as we did the simpler models to simulate an epidemic
+# And run it
+sol <- ode(y=yinit,times=seq(1950,2010,by=1),func=ja.multistage.model)
 
-sol <- ode(y=yinit,times=seq(0,100,by=0.1),func=ja.multistage.model)
+## Plot populations in each age group over time
+
+# First load the UN pop estimates and convert to long format
+UN_pop_age <- as.data.frame(read.table("SA_pop_age.txt",header=TRUE))
+UN_pop_age <- cbind(UN_pop_age,rowSums(UN_pop_age[,2:18]))
+colnames(UN_pop_age) <- c("time","0-4","5-9","10-14","15-19","20-24","25-29","30-34",
+                          "35-39","40-44","45-49","50-54","55-59","60-64","65-69","70-74","75-79","80+","Total")
+temp_data <- melt(UN_pop_age,id="time")
+
+# then turn the model output into long format
+temp <- rowSums(sol[,2:18])
+temp_model <- as.data.frame(cbind(sol,temp))
+colnames(temp_model) <- colnames(UN_pop_age)
+temp_model <- melt(temp_model,id="time")
+
+# and plot
+plot_temp <- ggplot(temp_model,aes(x=time,y=value))+
+                    geom_line(colour="red")+
+                    geom_point(data=temp_data,aes(x=time,y=value))+
+                    facet_wrap(~variable,scales="free")
 
 
-time <- sol[,1]
-infects <- sol[,1+iindex]
-plot(time,apply(infects,1,sum),type='l')
 
