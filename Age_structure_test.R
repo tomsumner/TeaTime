fdfe7e6c41ab007e588921aa938fae3f48b1ecde
tomsumner <tom.sumner@lshtm.ac.uk>
2015-02-02 17:35:52 +0000

    Updated to single year age bands - need to correct mortality

--- Age_structure_test.R
+++ Age_structure_test.R
@@ -11,13 +11,20 @@ library(reshape2)
 library(ggplot2)
 
 # Set up age structure
-ages <- c(seq(4,79,by=5),100) # upper end of age classes: 0-4 then five year bions to 79 then 80+
+ages <- seq(1,101,by=1) # upper end of age classes: 1 year age bins to 100
 num_ages <- length(ages) # calculates the number of age classes
 da <- diff(c(0,ages)) # calculates the widths of age classes
 
-## Initial conditions - 1950 population 
-yinit <- c(S=c(2088,1706,1485,1340,1183,1040,960,835,725,589,502,415,324,228,148,77,37))
+## Initial conditions - 1950 population
+UN_pop_age <- as.data.frame(read.table("SA_pop_age.txt",header=TRUE)) # Load UN Population data
+temp <- c()
+for (i in 1:101){temp[i]<-UN_pop_age[1,i+1]}
+yinit <- c(S=c(temp))
 sindex <- 1:num_ages
+
+
+
+
 
 ## To capture the aging process, define a matrix to hold the rates of movement between age classes.
 aging <- diag(-1/da)
@@ -26,12 +33,12 @@ aging[num_ages,num_ages]=0   # set rate out of last box = 0 - will account for f
 
 ## Mortality rates
 # Uses age specific LE tables from UN pop for South Africa, currently just the average over time, but could be made to vary with time
-LE<-c(55.00,50.60,46.02,41.64,37.51,33.66,29.94,26.44,23.01,19.73,16.65,13.75,11.22,9.04,7.20,5.62,3.09)
-death <- diag(1/LE)
+#LE<-c(55.00)
+#death <- diag(1/LE)
 
 ## Fertility
 # Uses crude birth rate (per 1000 population) from UN pop for South Africa, currently takes values at lower time band and linearly interpolates 
-births <- approxfun(x=c(1950,1955,1960,1965,1970,1975,1980,1985,1990,1995,2000,2005),y=c(44,42,41,38,38,36,34,31,27,25,24,22),rule=2)
+births <- approxfun(x=c(1952.5,1957.5,1962.5,1967.5,1972.7,1977.5,1982.5,1987.5,1992.5,1997.5,2002.5,2007.5),y=c(44,42,41,38,38,36,34,31,27,25,24,22),rule=2)
 
 ## Now we can put the pieces together to write a simulator for the demogrpahics
 ja.multistage.model <- function (t, x, ...) {
@@ -39,9 +46,9 @@ ja.multistage.model <- function (t, x, ...) {
 
   Total <- sum(s)
   
-  dsdt <- aging%*%s - death%*%s
+  dsdt <- aging%*%s #- death%*%s
   
-  dsdt[1] <- dsdt[1]+births(t)*Total/1000
+  #dsdt[1] <- dsdt[1]+births(t)*Total/1000
   
   list(c(dsdt),Total=Total)
 }
@@ -51,16 +58,13 @@ sol <- ode(y=yinit,times=seq(1950,2010,by=1),func=ja.multistage.model)
 
 ## Plot populations in each age group over time
 
-# First load the UN pop estimates and convert to long format
-UN_pop_age <- as.data.frame(read.table("SA_pop_age.txt",header=TRUE))
-UN_pop_age <- cbind(UN_pop_age,rowSums(UN_pop_age[,2:18]))
-colnames(UN_pop_age) <- c("time","0-4","5-9","10-14","15-19","20-24","25-29","30-34",
-                          "35-39","40-44","45-49","50-54","55-59","60-64","65-69","70-74","75-79","80+","Total")
+# add total to data and convert to long format
+UN_pop_age <- cbind(UN_pop_age,rowSums(UN_pop_age[,2:102]))
+colnames(UN_pop_age) <- c("time",seq(0,100,by=1),"Total")
 temp_data <- melt(UN_pop_age,id="time")
 
 # then turn the model output into long format
-temp <- rowSums(sol[,2:18])
-temp_model <- as.data.frame(cbind(sol,temp))
+temp_model <- as.data.frame(sol)
 colnames(temp_model) <- colnames(UN_pop_age)
 temp_model <- melt(temp_model,id="time")
 
