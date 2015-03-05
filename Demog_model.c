13395a67aa5765953e7547c542d2776c6f27a6c8
tomsumner <tom.sumner@lshtm.ac.uk>
2015-03-05 17:49:25 +0000

    Demog mdoel in C - works and matches R

--- Demog_model.c
+++ Demog_model.c
@@ -77,24 +77,13 @@ void derivsc(int *neq, double *t, double *y, double *ydot, double *yout, int *ip
 {
     if (ip[0] <2) error("nout should be at least 2");
     
+    int i=0; 
     double Total = y[0]+y[1]+y[2]+y[3]+y[4]+y[5]+y[6]+y[7]+y[8]+y[9]+y[10]+y[11]+y[12]+y[13]+y[14]+y[15]+y[16];
  
     ydot[0] = s_birth*birth_rate*Total/1000 - d*y[0];
-    ydot[1] = d*s5*y[0] - d*y[1];
-    ydot[2] = d*s10*y[1] - d*y[2];
-    ydot[3] = d*s15*y[2] - d*y[3];
-    ydot[4] = d*s20*y[3] - d*y[4];
-    ydot[5] = d*s25*y[4] - d*y[5];
-    ydot[6] = d*s30*y[5] - d*y[6];
-    ydot[7] = d*s35*y[6] - d*y[7];
-    ydot[8] = d*s40*y[7] - d*y[8];
-    ydot[9] = d*s45*y[8] - d*y[9];
-    ydot[10] = d*s50*y[9] - d*y[10];
-    ydot[11] = d*s55*y[10] - d*y[11];
-    ydot[12] = d*s60*y[11] - d*y[12];
-    ydot[13] = d*s65*y[12] - d*y[13];
-    ydot[14] = d*s70*y[13] - d*y[14];
-    ydot[15] = d*s75*y[14] - d*y[15];
+
+    for (i=1; i<16; i++) ydot[i] = d*forc[i+1]*y[i-1] - d*y[i];
+      
     ydot[16] = d*s80*y[15] - d2*y[16];
 
     yout[0] = Total;
